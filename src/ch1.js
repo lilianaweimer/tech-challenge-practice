7d2b60c948ed026dcaa830b0ff13aea38540a6a5
Liliana Weimer <lilianaweimer11@gmail.com>
2020-10-19 15:51:31 -0600

    :construction: Add test for URLify from Chapter One

--- src/ch1.js
+++ src/ch1.js
@@ -9,14 +9,32 @@ const chapterOne = {
   CHALLENGE
     What if you cannot use additional data structures?
   */
-  isUnique: () => {},
+  isUnique: (str) => {},
 
   /* 
   CHECK PERMUTATION
     Given two strings, write a method to decide
     if one is a permutation of the other. 
   */
- checkPermutation: () => {},
+ checkPermutation: (stringA, stringB) => {},
+
+  /* 
+  URLify
+    Write a method to replace all spaces in a string with '%20.'
+    You may assume that the string has sufficient space at the end
+    to hold the additional characters, and that you are given
+    the 'true' length of the string.
+  */
+ urlify: (str, chars) => {},
+
+  /* 
+  URLify
+    Write a method to replace all spaces in a string with '%20.'
+    You may assume that the string has sufficient space at the end
+    to hold the additional characters, and that you are given
+    the 'true' length of the string.
+  */
+ urlify: (str, chars) => {},
 
 };
 
--- test/ch1-test.js
+++ test/ch1-test.js
@@ -7,34 +7,53 @@ describe('Chapter One', () => {
 
   describe('Is Unique', () => {
 
-    it('should return true if there are no duplicates', () => {
+    it.skip('should return true if there are no duplicates', () => {
       const result = chapter.isUnique('qwertyuiop asdfghjkl');
       expect(result).to.equal(true);
     });
 
-    it('should return false if there is one duplicate', () => {
+    it.skip('should return false if there is one duplicate', () => {
       const result = chapter.isUnique('qweertyuiop');
       expect(result).to.equal(false);
     });
 
-    it('should return false if there are multiple duplicates', () => {
+    it.skip('should return false if there are multiple duplicates', () => {
       const result = chapter.isUnique('qweertyuuuioppp');
       expect(result).to.equal(false);
     });
 
+  });
+
   describe('Check Permutation', () => {
 
-    it('should return true if the second string contains a permutation of the first', () => {
+    it.skip('should return true if the second string contains a permutation of the first', () => {
       const result = chapter.checkPermutation('ab', 'balloon');
       expect(result).to.equal(true);
     });
 
-    it('should return false if the second string does NOT contain a permutation of the first', () => {
+    it.skip('should return false if the second string does NOT contain a permutation of the first', () => {
       const result = chapter.checkPermutation('ab', 'lampoon');
       expect(result).to.equal(false);
     });
 
   });
+
+  describe('URLify', () => {
+    
+    it.skip('should correctly transform sample string one', () => {
+      const result = chapter.urlify('this is a test');
+      expect(result).to.equal('this%20is%20a%20test');
+    });
+
+    it.skip('should correctly transform sample string two', () => {
+      const result = chapter.urlify(' another url  test ');
+      expect(result).to.equal('%20another%20url%20%20test%20');
+    });
+
+    it.skip('should not change strings with no spaces', () => {
+      const result = chapter.urlify('nospaces');
+      expect(result).to.equal('nospaces');
+    });
 
   });
 
